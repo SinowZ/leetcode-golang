de7ba0a1b743b0a3d80d9cd9641447afb24ada1b
SinowZ <sinow.zhang@gmail.com>
2020-04-03 16:46:58 +0800

    Update 118.go

--- 118.go
+++ 118.go
@@ -18,18 +18,19 @@ func generate(numRows int) [][]int {
 		return [][]int{{1}}
 	}
 
-	var ret = make([][]int, numRows)
+	ret := make([][]int, numRows)
 	ret[0] = []int{1}
+
 	for i := 1; i < numRows; i++ {
-		var tmp = make([]int, i+1)
+		temp := make([]int, i+1)
 		for j := 0; j <= i; j++ {
 			if j == 0 || j == i {
-				tmp[j] = 1
+				temp[j] = 1
 				continue
 			}
-			tmp[j] = ret[i-1][j] + ret[i-1][j-1]
+			temp[j] = ret[i-1][j-1] + ret[i-1][j]
 		}
-		ret[i] = tmp
+		ret[i] = temp
 	}
 	return ret
 }
