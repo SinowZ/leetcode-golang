e0f3d0593bfaa4e238bdfe33b018b02f6d9aeb47
SinowZ <sinow.zhang@gmail.com>
2020-04-22 17:55:26 +0800

    Create 28.go

---
+++ 28.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	ret := strStr("mississippi", "sipp")
+	fmt.Println("ret= ", ret)
+}
+
+func strStr(haystack string, needle string) int {
+	ret := -1
+	if needle == "" || needle == haystack {
+		return 0
+	}
+	if len(needle) > len(haystack) {
+		return ret
+	}
+	isThis := false
+	for i := 0; i <= len(haystack)-len(needle); i++ {
+		if haystack[i] == needle[0] {
+			for j := 0; j < len(needle); j++ {
+				isThis = true
+				if needle[j] != haystack[i+j] {
+					isThis = false
+					break
+				}
+			}
+			if isThis {
+				return i
+			}
+		}
+	}
+	return ret
+}
